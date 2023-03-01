f123f4172fca3948ebcddfe4f739d0b78453affd
F00570979 <122316489+F00570979@users.noreply.github.com>
2023-02-28 21:36:16 -0600

    Add project files.

---
+++ ArrayImplementation/ArrayImplementation.cpp
@@ -0,0 +1,59 @@
+// ArrayImplementation.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int size = 0, k = 0;
+    int sparseMatrix[4][5] =
+    {
+        {0 , 0 , 3 , 0 , 4 },
+        {0 , 0 , 5 , 7 , 0 },
+        {0 , 0 , 0 , 0 , 0 },
+        {0 , 2 , 6 , 0 , 0 }
+    };
+
+    
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (sparseMatrix[i][j] != 0)
+            {
+                size++;
+            }
+
+        }
+    }        
+
+    int compactMatrix[3][size];
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (sparseMatrix[i][j] != 0)
+            {
+                compactMatrix[0][k] = i;
+                compactMatrix[1][k] = j;
+                compactMatrix[2][k] = sparseMatrix[i][j];
+                k++;
+            }
+
+        }
+    }
+             
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            cout << " " << compactMatrix[i][j];
+        }
+        cout << "\n";
+    }
+    return 0;
+}
